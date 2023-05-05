34d9b7e7371d8eb56a21e4522a5ae27c10b85fe8
srsoumya96 <57213758+srsoumya96@users.noreply.github.com>
2023-05-05 19:13:43 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,44 @@ import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { LandingComponentComponent } from './landing-component/landing-component.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { EnquiryFormComponent } from './enquiry-form/enquiry-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+
+registerLocaleData(en);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
     LandingComponentComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    EnquiryFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NzModalModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

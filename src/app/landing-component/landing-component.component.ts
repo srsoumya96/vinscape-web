dcf7f9addbd32a309750f0410ae144cb9d6792e8
srsoumya96 <57213758+srsoumya96@users.noreply.github.com>
2023-05-05 19:18:51 +0530

    Update landing-component.component.ts

--- src/app/landing-component/landing-component.component.ts
+++ src/app/landing-component/landing-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EnquiryFormComponent } from '../enquiry-form/enquiry-form.component';
 
 @Component({
   selector: 'app-landing-component',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class LandingComponentComponent {
 
+  isVisible = false;
+  notifyMe() {
+    this.isVisible = true;
+  }
+
+  handleCancel(value:boolean) {
+    this.isVisible = false;
+  }
+
+  handleOk(form:any) {
+    fetch("https://api.apispreadsheets.com/data/Ldrj6qkoq4bICFTd/", {
+      method: "POST",
+      body: JSON.stringify({"data": {"EmailID": form.emailID, "Artist": form.isArtist}}),
+    }).then(res =>{
+      if (res.status === 201){
+        // SUCCESS
+        console.log("Success");
+        this.isVisible = false;
+      }
+      else{
+        // ERROR
+        console.log("Error");
+      }
+    })
+  }
 }
